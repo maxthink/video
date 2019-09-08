0f72e5daccee1bb5e3c741d73245182042ff5559
maxthink <maxthink@live.com>
2019-09-09 00:13:31 +0800

    juji is insert database

--- public/test/querylist.php
+++ public/test/querylist.php
@@ -1,5 +1,5 @@
 <?php
-
+set_time_limit(0);
 require '../../vendor/autoload.php';
 
 use QL\QueryList;
@@ -28,7 +28,8 @@ function geturl($url)
         )->getHtml();
     }catch(\Exception $e){
         echo $e->getMessage();
-        die;
+        //die;
+        return false;
     }
 }
 
@@ -36,14 +37,15 @@ function geturl($url)
 $html = geturl('/dianshiju/index.html');
 $data = QueryList::html($html)->find('.ui-pages>a')->attrs('href');
 $page_lists = $data->all();
-print_r($page_lists);
+//print_r($page_lists);
 
 for($i=1;$i<count($page_lists);$i++){
     
     $html = geturl($page_lists[$i]);
+    if(false !== $html){
+        $data = QueryList::html($html)->find('.ui-pages>a')->attrs('href');
+    }else{ continue; }
     
-    
-    $data = QueryList::html($html)->find('.ui-pages>a')->attrs('href');
     $_lists = $data->all();
     foreach($_lists as $_url){
         if(!in_array($_url,$page_lists)){
@@ -59,6 +61,7 @@ natsort($page_lists);
 $detail = [];
 foreach($page_lists as $list_url){
     $html = geturl($list_url);
+    if(false == $html) continue;
     //列表页的单个元素
     $rule=[
         'name'=>['.play-txt>h5>a ','text'],
@@ -76,22 +79,69 @@ foreach($page_lists as $list_url){
             $detail[ $unit['url'] ] = $unit;
         }
     }
-    //print_r($detail);die;
+    
 }
 
 //todo 进详情页
 foreach($detail as $k=>$unit )
 {
+    echo '<br/>'.$unit['url'];
     $html = geturl($unit['url']);
+    
     $rule = ['sid'=>['#SOHUCS','sid']];
-    $sid = QueryList::html($html)->find('#SOHUCS')->attr('sid');
-
-    $unit['id']=$sid;
-    $unit['res'] = $unit['url'].'play-'.$sid.'-0-1.js';
-    $html = geturl($unit['res']);
-    $unit['res_js']= json_decode( substr( str_replace('var ff_urls=\'', '', $html),0,-2),1);
-    print_r($unit);die;
+    try{
+        $sid = QueryList::html($html)->find('#SOHUCS')->attr('sid');
+    }catch(Exception $e){
+        unset($detail[$k]);
+        continue;
+    }
+    $unit['res_id']=$sid;
+    
+    $html = geturl( $unit['url'].'play-'.$sid.'-0-1.js' );
+    
+    $unit['res_js']= substr( str_replace('var ff_urls=\'', '', $html),0,-2);
+    //print_r($unit);die;
     $detail[$k]=$unit;
     
 }
+print_r($detail);
 
+$dbhost = 'localhost';
+$dbname = 'video';
+$dbuser = 'video';
+$dbpass = 'guoqu123!@#';
+
+$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+if($mysqli->connect_error){
+        log('connect error:'.$mysqli->connect_errno);
+        die( 'connect error:'.$mysqli->connect_errno);
+}
+$mysqli->set_charset('UTF-8'); // 设置数据库字符集
+//$mysqli->autocommit(FALSE);
+
+$stmt = $mysqli->prepare('insert h_video (`res_id`,`name`,`cover`,`actor`,`type`,`url`,`res_js`,`md5`) values (?,?,?,?,?,?,?,?)');
+
+$stmt->bind_param('isssssss',$res_id,$name,$cover,$actor,$type,$url,$res_js,$md5);
+
+foreach($detail as $video){
+    if(!isset($video['res_id'])) continue;
+    $k = 'select res_id from h_video where res_id = '.$video['res_id'];
+    $res = $mysqli->query($k);
+    $res = mysqli_fetch_row($res);
+    
+    if( isset($res[0]) && $res[0]==$video['res_id'] ) continue;
+        
+    $res_id = $video['res_id'];
+    $name = $video['name'];
+    $cover = $video['cover'];
+    $actor = $video['actor'];
+    $type = $video['type'];
+    $url = $video['url'];
+    $res_js = $video['res_js'];
+    $md5 = md5($video['res_js']);
+    
+    $stmt->execute();
+
+}
+//$mysqli->query('commit');
+$stmt->close();
